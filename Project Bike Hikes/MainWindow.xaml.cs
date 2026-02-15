afe46c64098452de8a5528ce72f2be13e658c32e
Valkorion <ezrared3@gmail.com>
2026-02-15 16:20:59 +0000

    commit soutenance

--- Project Bike Hikes/MainWindow.xaml.cs
+++ Project Bike Hikes/MainWindow.xaml.cs
@@ -1,36 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-
-// <Image Source="/Ressource/18086706-.jpg" Width="800" Height="450"/>
 
 namespace Project_Bike_Hikes
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    /// 
 
-    public enum Difficulty {
+    public enum Difficulty
+    {
         Easy,
         Low,
         Medium,
         Hard,
         Impossible
     }
-    public enum BikeType {
+    public enum BikeType
+    {
         Road,
         Mountain,
         Hybrid,
@@ -39,7 +26,8 @@ namespace Project_Bike_Hikes
         BMX
     }
 
-    public enum Crowds {
+    public enum Crowds
+    {
         Nobody,
         Some,
         Average,
@@ -47,7 +35,8 @@ namespace Project_Bike_Hikes
         Impossible
     }
 
-    public enum Weather {
+    public enum Weather
+    {
         Sunny,
         Cloudy,
         Rainy,
@@ -72,7 +61,8 @@ namespace Project_Bike_Hikes
         {
         }
 
-        public Ride(string name, List<Difficulty> diff, List<BikeType> types, List<Crowds> crowds, List<Weather> weather, string bestTime, Image map) {
+        public Ride(string name, List<Difficulty> diff, List<BikeType> types, List<Crowds> crowds, List<Weather> weather, string bestTime, Image map)
+        {
 
             Name = name;
             Diff = diff;
@@ -81,6 +71,7 @@ namespace Project_Bike_Hikes
             Weather = weather;
             BestTime = bestTime;
             Map = map;
+
 
         }
 
@@ -95,18 +86,177 @@ namespace Project_Bike_Hikes
         public MainWindow()
         {
             InitializeComponent();
+            DiffBox.ItemsSource = Enum.GetValues(typeof(Difficulty));
+            DiffBox_Copy.ItemsSource = Enum.GetValues(typeof(BikeType));
+            DiffBox_Copy1.ItemsSource = Enum.GetValues(typeof(Weather));
+            DiffBox_Copy3.ItemsSource = Enum.GetValues(typeof(Crowds));
+
 
 
         }
 
+        List<Ride> list;
+
         private void Window_Loaded_1(object sender, RoutedEventArgs e)
         {
 
-            List<Ride> list = new List<Ride>();
+            BackgroundVideo.Play();
 
-            Ride r1 = new Ride() { Name = "Ride 1", Diff = new List<Difficulty> { Difficulty.Easy, Difficulty.Medium }, Type = new List<BikeType> { BikeType.Gravel, BikeType.Mountain }, Crowds = new List<Crowds> { Crowds.Some }, Weather = new List<Weather> { Weather.Sunny }, BestTime = "10AM to 8PM", Map = new Image() };
+           list = new List<Ride>();
+
+            Ride r1 = new Ride()
+            {
+                Name = "Coastal Breeze Ride",
+                Diff = new List<Difficulty> { Difficulty.Easy, Difficulty.Low },
+                Type = new List<BikeType> { BikeType.Road, BikeType.Hybrid },
+                Crowds = new List<Crowds> { Crowds.Some },
+                Weather = new List<Weather> { Weather.Sunny, Weather.Cloudy },
+                BestTime = "8AM to 7PM",
+                Map = new Image()
+            };
+
+            Ride r2 = new Ride()
+            {
+                Name = "Mountain Peak Challenge",
+                Diff = new List<Difficulty> { Difficulty.Hard, Difficulty.Impossible },
+                Type = new List<BikeType> { BikeType.Mountain, BikeType.Electric },
+                Crowds = new List<Crowds> { Crowds.Nobody, Crowds.Some },
+                Weather = new List<Weather> { Weather.Cold, Weather.Foggy },
+                BestTime = "6AM to 3PM",
+                Map = new Image()
+            };
+
+            Ride r3 = new Ride()
+            {
+                Name = "Forest Gravel Loop",
+                Diff = new List<Difficulty> { Difficulty.Medium, Difficulty.Hard },
+                Type = new List<BikeType> { BikeType.Gravel, BikeType.Mountain },
+                Crowds = new List<Crowds> { Crowds.Average },
+                Weather = new List<Weather> { Weather.Cloudy, Weather.Humid },
+                BestTime = "9AM to 6PM",
+                Map = new Image()
+            };
+
+            Ride r4 = new Ride()
+            {
+                Name = "City Sunrise Ride",
+                Diff = new List<Difficulty> { Difficulty.Easy },
+                Type = new List<BikeType> { BikeType.Road, BikeType.Electric },
+                Crowds = new List<Crowds> { Crowds.Lot },
+                Weather = new List<Weather> { Weather.Sunny, Weather.Cloudy },
+                BestTime = "5AM to 9AM",
+                Map = new Image()
+            };
+
+            Ride r5 = new Ride()
+            {
+                Name = "Desert Heat Trail",
+                Diff = new List<Difficulty> { Difficulty.Medium, Difficulty.Hard },
+                Type = new List<BikeType> { BikeType.Mountain, BikeType.Gravel },
+                Crowds = new List<Crowds> { Crowds.Nobody },
+                Weather = new List<Weather> { Weather.Hot, Weather.Sunny },
+                BestTime = "6AM to 11AM",
+                Map = new Image()
+            };
+
+            Ride r6 = new Ride()
+            {
+                Name = "Foggy Cliffside Route",
+                Diff = new List<Difficulty> { Difficulty.Medium },
+                Type = new List<BikeType> { BikeType.Road, BikeType.Gravel },
+                Crowds = new List<Crowds> { Crowds.Some },
+                Weather = new List<Weather> { Weather.Foggy, Weather.Cloudy },
+                BestTime = "10AM to 5PM",
+                Map = new Image()
+            };
+
+            Ride r7 = new Ride()
+            {
+                Name = "Forest Adventure MTB",
+                Diff = new List<Difficulty> { Difficulty.Medium, Difficulty.Hard },
+                Type = new List<BikeType> { BikeType.Mountain },
+                Crowds = new List<Crowds> { Crowds.Some, Crowds.Nobody },
+                Weather = new List<Weather> { Weather.Cloudy, Weather.Rainy },
+                BestTime = "9AM to 4PM",
+                Map = new Image()
+            };
+
+            Ride r8 = new Ride()
+            {
+                Name = "Riverside Zen Path",
+                Diff = new List<Difficulty> { Difficulty.Easy },
+                Type = new List<BikeType> { BikeType.Hybrid, BikeType.Road },
+                Crowds = new List<Crowds> { Crowds.Average },
+                Weather = new List<Weather> { Weather.Sunny, Weather.Cloudy },
+                BestTime = "11AM to 8PM",
+                Map = new Image()
+            };
+
+            Ride r9 = new Ride()
+            {
+                Name = "Urban Night Ride",
+                Diff = new List<Difficulty> { Difficulty.Low, Difficulty.Medium },
+                Type = new List<BikeType> { BikeType.Road, BikeType.Electric },
+                Crowds = new List<Crowds> { Crowds.Lot, Crowds.Impossible },
+                Weather = new List<Weather> { Weather.Cloudy, Weather.Humid },
+                BestTime = "7PM to 11PM",
+                Map = new Image()
+            };
+
+            Ride r10 = new Ride()
+            {
+                Name = "BMX Park Fun Line",
+                Diff = new List<Difficulty> { Difficulty.Medium, Difficulty.Hard },
+                Type = new List<BikeType> { BikeType.BMX },
+                Crowds = new List<Crowds> { Crowds.Average, Crowds.Lot },
+                Weather = new List<Weather> { Weather.Sunny, Weather.Cold },
+                BestTime = "12PM to 6PM",
+                Map = new Image()
+            };
 
             list.Add(r1);
+            list.Add(r2);
+            list.Add(r3);
+            list.Add(r4);
+            list.Add(r5);
+            list.Add(r6);
+            list.Add(r7);
+            list.Add(r8);
+            list.Add(r9);
+            list.Add(r10);
+
+            DiffBox.SelectionChanged += FilterRides;
+            DiffBox_Copy.SelectionChanged += FilterRides;
+            DiffBox_Copy1.SelectionChanged += FilterRides;
+            DiffBox_Copy3.SelectionChanged += FilterRides;
+
+        }
+
+        private void FilterRides(object sender, SelectionChangedEventArgs e)
+        {
+            if (list == null || list.Count == 0)
+                return;
+
+            var filtered = list.AsEnumerable();
+
+            
+            if (DiffBox.SelectedItem is Difficulty diff)
+                filtered = filtered.Where(r => r.Diff.Contains(diff));
+
+            
+            if (DiffBox_Copy.SelectedItem is BikeType type)
+                filtered = filtered.Where(r => r.Type.Contains(type));
+
+            
+            if (DiffBox_Copy1.SelectedItem is Weather weather)
+                filtered = filtered.Where(r => r.Weather.Contains(weather));
+
+            
+            if (DiffBox_Copy3.SelectedItem is Crowds crowd)
+                filtered = filtered.Where(r => r.Crowds.Contains(crowd));
+
+            
+            BestMatchListBox.ItemsSource = filtered.Select(r => r.Name).ToList();
         }
 
         private void DiffBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -114,8 +264,13 @@ namespace Project_Bike_Hikes
 
         }
 
- 
+        private void BackgroundVideo_MediaEnded(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("Video ended");
+            BackgroundVideo.Position = TimeSpan.Zero;
+            BackgroundVideo.Play();
+            Console.WriteLine("Video replay");
+        }
     }
 
 }
-
